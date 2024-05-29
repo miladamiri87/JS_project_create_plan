8ebe6320eb82b05d612f898f3d0566ec7d4fc60a
miladamiri87 <45160585+miladamiri87@users.noreply.github.com>
2024-05-29 17:29:15 +0330

    create line Vertical Or Horizontal
    you can create line vertical or horizontal with add line VH btn

--- main.js
+++ main.js
@@ -3,22 +3,18 @@ var canvas = new fabric.Canvas("canvas", {
   height: window.innerHeight,
 });
 
-
 //define button
 let adding_line_btn = document.getElementById("adding_line_btn");
-let adding_VH_line_btn = document.getElementById("adding_VH_line_btn")
+let adding_VH_line_btn = document.getElementById("adding_VH_line_btn");
 let stop_btn = document.getElementById("stop");
 let clear_btn = document.getElementById("clear");
 //end of define button
-
-
 
 //define function for each btn
 adding_line_btn.addEventListener("click", activateAddingLine);
 stop_btn.addEventListener("click", stopAddingLine);
 clear_btn.addEventListener("click", clear_canvas);
-adding_VH_line_btn.addEventListener('click', activateAddingLine_VH)//switch canvas on for create line VH
-
+adding_VH_line_btn.addEventListener("click", activateAddingLine_VH); //switch canvas on for create line VH
 
 var mouseDown = true; //key for stop drawing
 var line;
@@ -37,7 +33,6 @@ var polyLine = new fabric.Polyline(points, {
   hasBorders: false,
   evented: true,
   targetFindTolerance: true,
-
 });
 
 //add polyline to canvas
@@ -46,18 +41,14 @@ setPolyCoords();
 canvas.selection = false;
 canvas.renderAll();
 
-
-
-
 function activateAddingLine_VH() {
-  canvas.on("mouse:down", startAddingLine_VH);//function for create VH line
+  canvas.on("mouse:down", startAddingLine_VH); //function for create VH line
 }
 function activateAddingLine() {
-  canvas.on("mouse:down", startAddingLine);//function for create free line
+  canvas.on("mouse:down", startAddingLine); //function for create free line
 }
 
-
-var count = 0
+var count = 0;
 var perv_Point_y = 0;
 var perv_Point_x = 0;
 var x_horizontal;
@@ -67,29 +58,85 @@ var y_vertical;
 var isHorizontal;
 
 function startAddingLine_VH(o) {
-
-
   let pointer = canvas.getPointer(o.e);
-  var newPoint_x = pointer.x
-  var newPoint_y = pointer.y
-  if(count>0){
-    console.log(perv_Point_x, newPoint_x)
+  var newPoint_x = pointer.x;
+  var newPoint_y = pointer.y;
+  if (count > 0) {
+    //dovomin click
 
+    console.log(perv_Point_x, newPoint_x);
+
+    /////----------test is Horizontal-----------------------------------------------------------------------------------
+
+    if (newPoint_y > perv_Point_y) {
+      var angle = Math.abs(
+        Math.atan2(newPoint_y - perv_Point_y, newPoint_x - perv_Point_x)
+      );
+      if (angle < Math.PI / 4 || angle > (3 * Math.PI) / 4) {
+        console.log(angle);
+        isHorizontal = true;
+      } else if (angle > Math.PI / 4 || angle < (3 * Math.PI) / 4) {
+        isHorizontal = false;
+        console.log(angle);
+      }
+    } else {
+      var angle = Math.abs(
+        Math.atan2(newPoint_x - perv_Point_x, newPoint_y - perv_Point_y)
+      );
+      if (angle < Math.PI / 4 || angle > (3 * Math.PI) / 4) {
+        console.log(angle);
+        isHorizontal = false;
+      } else if (angle > Math.PI / 4 || angle < (3 * Math.PI) / 4) {
+        isHorizontal = true;
+        console.log(angle);
+      }
+    }
+    console.log(isHorizontal);
+
+    /////----------End test is Horizontal-----------------------------------------------------------------------------------
+
+    /////----------create a line Horizontal-----------------------------------------------------------------------------------
+
+    if (isHorizontal) {
+      polyLine.points.push(new fabric.Point(pointer.x, perv_Point_y));
+      setPolyCoordsVH();
+      document.getElementById("lenght").innerText =
+        Math.round(Math.abs((newPoint_x - perv_Point_x) * 0.0264583333)) + "cm";
+      polyLine.dirty = true;
+      canvas.add(polyLine);
+      canvas.renderAll();
+    }
+
+    /////----------create a line vertical-----------------------------------------------------------------------------------
+    else {
+      polyLine.points.push(new fabric.Point(perv_Point_x, newPoint_y));
+      setPolyCoordsVH();
+      document.getElementById("lenght").innerText =
+        Math.round(Math.abs((newPoint_y - perv_Point_y) * 0.0264583333)) + "cm";
+      polyLine.dirty = true;
+      canvas.add(polyLine);
+      canvas.renderAll();
+    }
+    perv_Point_x = canvas.getPointer(o.e).x;
+    perv_Point_y = canvas.getPointer(o.e).y;
+    count += 1;
   }
+      /////----------End vertical/horizontal line-----------------------------------------------------------------------------------
 
+  
+  else {  //avalin click
 
-  polyLine.points.push(new fabric.Point(pointer.x, pointer.y));
-  setPolyCoordsVH();
-  document.getElementById('lenght').innerText = Math.round(Math.abs((pointer.x - pointer.y) * 0.0264583333)) + 'cm'
-  // polyLine.dirty = true;
-  canvas.add(polyLine);
-  canvas.renderAll();
-  perv_Point_x = canvas.getPointer(o.e).x;
-  perv_Point_y = canvas.getPointer(o.e).y;
-  count += 1
-  // console.log(perv_Point_x)
-  // console.log(perv_Point_y)
-
+    polyLine.points.push(new fabric.Point(pointer.x, pointer.y));
+    setPolyCoordsVH();
+    document.getElementById("lenght").innerText =
+      Math.round(Math.abs((pointer.x - pointer.y) * 0.0264583333)) + "cm";
+    polyLine.dirty = true;
+    canvas.add(polyLine);
+    canvas.renderAll();
+    perv_Point_x = pointer.x;
+    perv_Point_y = pointer.y;
+    count += 1;
+  }
 }
 
 function setPolyCoordsVH() {
@@ -108,15 +155,15 @@ function setPolyCoordsVH() {
   canvas.renderAll();
 }
 
-
 // --------------------------------------------------------------------------------------------------------------
 
 function startAddingLine(o) {
   let pointer = canvas.getPointer(o.e);
   polyLine.points.push(new fabric.Point(pointer.x, pointer.y));
-  console.log(polyLine)
+  console.log(polyLine);
   setPolyCoords();
-  document.getElementById('lenght').innerText = Math.round(Math.abs((pointer.x - pointer.y) * 0.0264583333)) + 'cm'
+  document.getElementById("lenght").innerText =
+    Math.round(Math.abs((pointer.x - pointer.y) * 0.0264583333)) + "cm";
   polyLine.dirty = true;
   canvas.add(polyLine);
   canvas.renderAll();
@@ -132,15 +179,16 @@ function setPolyCoords() {
       y: polyLine.minY + polyLine.height / 2,
     },
   });
-  console.log(polyLine.minX + polyLine.width / 2, polyLine.minY + polyLine.height / 2)
+  console.log(
+    polyLine.minX + polyLine.width / 2,
+    polyLine.minY + polyLine.height / 2
+  );
   polyLine.dirty = true;
   polyLine.setCoords();
   canvas.renderAll();
 }
 
-
 // --------------------------------------------------------------------------------------------------------------
-
 
 //stop creat line process
 function stopAddingLine() {
@@ -149,18 +197,18 @@ function stopAddingLine() {
   canvas.off("mouse:down", startAddingLine_VH);
   canvas.selection = false;
   polyLine.selection = false;
-  count = 0
-  perv_Point_x = 0
-  perv_Point_y = 0
+  count = 0;
+  perv_Point_x = 0;
+  perv_Point_y = 0;
 }
 
 //clear canvas
 function clear_canvas() {
-  console.log('s')
+  console.log("s");
   points = [];
-  count = 0
-  perv_Point_x = 0
-  perv_Point_y = 0
+  count = 0;
+  perv_Point_x = 0;
+  perv_Point_y = 0;
   canvas.dispose();
   polyLine = new fabric.Polyline(points, {
     stroke: "green",
@@ -174,12 +222,9 @@ function clear_canvas() {
     hasBorders: false,
     evented: true,
     targetFindTolerance: true,
-
   });
   canvas = new fabric.Canvas("canvas", {
     width: window.innerWidth,
     height: window.innerHeight,
   });
 }
-
-
